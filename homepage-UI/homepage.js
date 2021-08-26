3a431c050f6d1e700822d6af86bb360e9dead662
Mukul <howalemukul@gmail.com>
2021-08-26 15:27:19 +0530

    update on simple steps and causes div

--- homepage-UI/homepage.js
+++ homepage-UI/homepage.js
@@ -1,13 +1,28 @@
-// change background color of button
-var startFund = document.getElementById('start-fundraiser');
+// change background color of button talk button
+var talk  = document.getElementById('talk');
 
-startFund.onmouseover = function(){
-    startFund.style.backgroundColor = "#039695";
-    startFund.style.cursor = "pointer";
+talk.onmouseover = function(){
+    talk.style.backgroundColor = "#01BFBD";
+    talk.style.color = "rgb(255, 255, 255)";
 }
 
-startFund.onmouseleave = function(){
-    startFund.style.backgroundColor = "#01BFBD";
+talk.onmouseleave = function(){
+    talk.style.backgroundColor = "white";
+    talk.style.color = "#01BFBD";
+}
+
+// change background color of button start button
+var startFund = document.getElementsByClassName('start-fundraiser');
+
+for(let i=0; i<startFund.length-1; i++){
+    startFund[i].onmouseover = function(){
+        startFund[i].style.backgroundColor = "#039695";
+        startFund[i].style.cursor = "pointer";
+    }
+
+    startFund[i].onmouseleave = function(){
+        startFund[i].style.backgroundColor = "#01BFBD";
+    }
 }
 
 // support button 
@@ -15,4 +30,14 @@ var support = document.getElementById('support');
 
 support.onmouseover = function(){
     support.style.cursor = "pointer";
+}
+
+//causes dynamic
+
+var causes = document.getElementById('causes-img').childNodes;
+
+for(let i=1; i<causes.length; i+=2){
+    causes[i].onmouseover = function(){
+        causes[i].style.cursor ="pointer";
+    }
 }
