43339a2cc0e077b8289581f6922cd5bfb55acfd1
Rahul <1996meenarahul@gmail.com>
2021-08-27 20:01:47 +0530

    latest commit

--- browse.js
+++ browse.js
@@ -24,7 +24,56 @@ var fund_raisers = [
     raised : 16800,
     goal :  300000
    },
-  
+   
+   {
+    title : "My 2.5year Kid Lost His Dad. Help Me To Payoff And In His Education And Upbringing.",
+    image : "https://kettocdn.gumlet.io/media/campaign/424000/424034/image/wid60b476cd93ce2.jpeg?w=300&dpr=1.3",
+    userImage : " ",
+    fundraiser : "Vartika Agarwal",
+    daysLeft : 6,
+    supporters : 241,
+    category : "Education",
+    raised : 2345650,
+    goal :  10000000
+   },
+   
+   {
+    title : " BACK TO SCHOOL",
+    image : "https://kettocdn.gumlet.io/media/campaign/445000/445343/image/wid60f6ea7fc6789.jpeg?w=300&dpr=1.3",
+    userImage : "https://kettocdn.gumlet.io/media/ngo/4244000/4244769/image/e745dbfec86a60c512fd3ee1348e29bf85b132a3.jpg?w=50&dpr=1.3",
+    fundraiser : "Vidya Bhawan Society",
+    daysLeft : 47,
+    supporters : 157,
+    category : "Education",
+    raised : 1167650,
+    goal :  15000000
+   },
+   
+   {
+    title : "Rebuilding 1500 Children’s Lives With Life Skills Education",
+    image : "https://kettocdn.gumlet.io/media/campaign/265000/265751/image/wid23be670883cbe43f3e2a5c74edb8c7f9403feaca.jpg?w=300&dpr=1.3",
+    userImage : "https://kettocdn.gumlet.io/media/ngo/97000/97237/image/deba88f96642896d7f22a837f59210436a57eb41.jpg?w=50&dpr=1.3",
+    fundraiser : "Enabling Leadership",
+    daysLeft : 27,
+    supporters : 47,
+    category : "Education",
+    raised : 1043650,
+    goal :  5000000
+   },
+   
+   {
+    title : "'Gift A Brick' To Build An Inclusive School In Karjat",
+    image : "https://kettocdn.gumlet.io/media/campaign/240000/240053/image/wid5f50d4ea5fa9a.jpeg?w=300&dpr=1.3",
+    userImage : "https://kettocdn.gumlet.io/media/ngo/1067000/1067364/image/a825f32d0e5cc529fca197c9866f92d5f1b0eaf6.JPG?w=50&dpr=1.3",
+    fundraiser : "Samaaveshi Pathshaala Foundation",
+    daysLeft : 34,
+    supporters : 135,
+    category : "Education",
+    raised : 673344,
+    goal :  1000000
+   },
+
+
    {
     title : "Support My Antarctic Expedition In March 2022 On Climate Leadership And Awareness Generation",
     image : "https://kettocdn.gumlet.io/media/campaign/336000/336158/image/wid603b9671b751d.jpeg?w=300&dpr=1.3",
@@ -47,7 +96,149 @@ var fund_raisers = [
     raised : 294731,
     goal :  600000
    }, 
-
+   {
+    title : "Udvada Village Waste Management & Community Clean Up",
+    image : "https://kettocdn.gumlet.io/media/campaign/231000/231964/image/wid5f47c75f06e34.png?w=300&dpr=1.3",
+    userImage : "https://kettocdn.gumlet.io/media/ngo/1942000/1942542/image/873400cdd1b9188d84f16fa40a9b1f8997187198.jpg?w=50&dpr=1.3",
+    fundraiser : "Clean And Green Charitable Trust",
+    daysLeft : 45,
+    supporters : 20,
+    category : "Environment",
+    raised : 154001,
+    goal :  1000000
+   }, 
+   {
+    title : "Support Hyacinth Clearing Of Jakkur Lake Handled By Fishermen",
+    image : "https://kettocdn.gumlet.io/media/campaign/383000/383590/image/wid608bc49c00917.jpeg?w=300&dpr=1.3",
+    userImage : "https://kettocdn.gumlet.io/media/individual/3912000/3912088/image/915f26b2cb10c54215aa72239ffafe7d8c582181.jpeg?w=50&dpr=1.3",
+    fundraiser : "Theertha Ravindran",
+    daysLeft : 62,
+    supporters : 13,
+    category : "Environment",
+    raised : 40000,
+    goal :  400000
+   }, 
+   {
+    title : "Help Us Build Our Native Nursery!",
+    image : "https://kettocdn.gumlet.io/media/campaign/429000/429940/image/wid60bdff304f4b5.jpg?w=300&dpr=1.3",
+    userImage : "https://kettocdn.gumlet.io/media/ngo/946000/946163/image/5f8d3bf1bd73f.jpg?w=50&dpr=1.3",
+    fundraiser : "Swechha We For Change Foundation",
+    daysLeft : 5,
+    supporters : 12,
+    category : "Environment",
+    raised : 31000,
+    goal :  1000000
+   }, 
+   {
+    title : "Help Us Create Green Lungspaces In Your Municipal Ward!",
+    image : "https://kettocdn.gumlet.io/media/campaign/464000/464383/image/wid60ebbbeca0191.jpeg?w=300&dpr=1.3",
+    userImage : "https://kettocdn.gumlet.io/media/ngo/1002000/1002098/image/611b704aae860.jpg?w=50&dpr=1.3",
+    fundraiser : "Team Hasiru",
+    daysLeft : 35,
+    supporters : 6,
+    category : "Environment",
+    raised : 20000,
+    goal : 165000
+   },
+   {
+    title : "Save The Sundarbans",
+    image : "https://kettocdn.gumlet.io/media/campaign/475000/475590/image/wid60fec69689608.jpeg?w=300&dpr=1.3",
+    userImage : "https://kettocdn.gumlet.io/media/individual/1159000/1159369/image/6112c1c048f9a.jpg?w=50&dpr=1.3",
+    fundraiser : "Sukanya Roy",
+    daysLeft : 15,
+    supporters : 6,
+    category : "Environment",
+    raised : 9500,
+    goal : 30000
+   },  
+   {
+    title : "Help Me Fund My FIFA Master Expenses And Pursue This Dream Degree",
+    image : "https://kettocdn.gumlet.io/media/campaign/366000/366181/image/wid606b078fd7a72.jpeg?w=300&dpr=1.3",
+    userImage : "https://kettocdn.gumlet.io/media/individual/3901000/3901004/image/6114f08c34d54.jpg?w=50&dpr=1.3",
+    fundraiser : "AISHA NAZIA NASIR MAYIN",
+    daysLeft : 35,
+    supporters : 148,
+    category : "Education",
+    raised : 1267650,
+    goal :  2800000
+   },
+   {
+    title : "My Little Boy’s Cancer Has Relapsed Twice But I’m Helpless. Please Save Him",
+    image : "https://kettocdn.gumlet.io/media/campaign/212000/212655/image/wid1f1a33100b0ce11487774ca00c4341f7d0364e69.jpg?w=300&dpr=1.3",
+    userImage : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTBezEM4aVLCSKM7Gta0gLZauALdyMBUPy69w&usqp=CAU",
+    fundraiser : "Family Friends",
+    daysLeft : 35,
+    supporters : 148,
+    category : "Medical",
+    raised : 103554,
+    goal :  5000000
+   },
+   {
+    title : "To Distribute Sanitary Pads To Poor Women Impacted By Covid/Corona Lockdown",
+    image : "https://kettocdn.gumlet.io/media/campaign/191000/191540/image/wid5eaab67e1520c.png?w=300&dpr=1.3",
+    userImage : "https://kettocdn.gumlet.io/media/ngo/701000/701043/image/612083829f12c.jpg?w=50&dpr=1.3",
+    fundraiser : "Pinkishe Foundation",
+    daysLeft : 55,
+    supporters : 478,
+    category : "Womens & Girls",
+    raised : 801554,
+    goal :  1000000
+   },
+   {
+    title : "Help Underprivileged Women With Sanitary Pads",
+    image : "https://kettocdn.gumlet.io/media/campaign/209000/209801/image/wid5ef0926136ba7.png?w=300&dpr=1.3",
+    userImage : "https://kettocdn.gumlet.io/media/individual/1721000/1721508/image/5f0032b1964b0.jpg?w=50&dpr=1.3",
+    fundraiser : "Niveda Ravikumar",
+    daysLeft : 5,
+    supporters :367,
+    category : "Womens & Girls",
+    raised : 302554,
+    goal :  500000
+   },
+   {
+    title : "URGENT FUNDS NEEDED To Maintain The Shelter Infrastructure",
+    image : "https://kettocdn.gumlet.io/media/campaign/235000/235613/image/wid5f4a0f79219ff.jpeg?w=300&dpr=1.3",
+    userImage : "https://kettocdn.gumlet.io/media/ngo/119000/119583/image/04b7395a816c9206a9129e7cb33dff0ac41117d8.jpg?w=50&dpr=1.3",
+    fundraiser : "People For Animals Agra",
+    daysLeft : 5,
+    supporters :367,
+    category : "Animals",
+    raised : 1802554,
+    goal :  2000000
+   },
+   {
+    title : "The Chennai Snake Park Has Run Out Of Funds. The Park & Its Animal Inhabitants Need Your Support To Survive The Pandemic.",
+    image : "https://kettocdn.gumlet.io/media/campaign/477000/477628/image/wid758c44aabd99cbda031f55c58f93720d45069c1e.jpg?w=300&dpr=1.3",
+    userImage : "https://kettocdn.gumlet.io/media/ngo/4739000/4739165/image/697364d91383a0115428b1f042eef6246f943dd7.jpg?w=50&dpr=1.3",
+    fundraiser : "Chennai Snake Park Trust",
+    daysLeft : 50,
+    supporters :567,
+    category : "Animals",
+    raised : 1452554,
+    goal :  5000000
+   },
+   {
+    title : "Help Build A Home For 1300 Dogs & 300 Cats",
+    image : "https://kettocdn.gumlet.io/media/campaign/238000/238917/image/wid5f57d3c692465.jpeg?w=300&dpr=1.3",
+    userImage : "https://kettocdn.gumlet.io/media/ngo/1957000/1957525/image/fe4c253079a19837e1ec7a2bb279c80f83cee943.jpg?w=50&dpr=1.3",
+    fundraiser : "Mission Possible Foundation",
+    daysLeft : 80,
+    supporters :597,
+    category : "Animals",
+    raised : 1252554,
+    goal :  4000000
+   },
+   {
+    title : "Cause For Paws By Heartfulness",
+    image : "https://kettocdn.gumlet.io/media/campaign/366000/366383/image/wid606c7b2675bf5.png?w=300&dpr=1.3",
+    userImage : "https://kettocdn.gumlet.io/media/ngo/3893000/3893404/image/fd80314345f3b51741130fea6dc8cfb921b2ed87.png?w=50&dpr=1.3",
+    fundraiser : "Heartfulness Institute",
+    daysLeft : 64,
+    supporters :245,
+    category : "Animals",
+    raised : 902554,
+    goal :  5000000
+   },
   ] 
  
 
@@ -59,6 +250,87 @@ var fund_raisers = [
       }
   }
   
+  let MyFundraisers = JSON.parse(localStorage.getItem("fund_raisers"));
+
+   window.onload = function(){
+
+    MyFundraisers.forEach( function(data){
+         let imageTag = document.createElement('img');
+         imageTag.src = data.image;
+
+         let titleTag = document.createElement('p');
+         titleTag.setAttribute('id','titleText');
+         titleTag.innerText = data.title;
+         
+         let userLogo = document.createElement('img');
+         userLogo.src = data.userImage;
+         userLogo.alt = "";
+         userLogo.style.borderRadius = "50%";
+         userLogo.style.width = "30px"
+         userLogo.style.height = "30px";
+         userLogo.style.marginLeft ="16px"
+          
+         
+         let raiser = document.createElement('p');
+         raiser.setAttribute('id','raiserText');
+         raiser.innerText = "by " + data.fundraiser;
+
+
+         let raisedFund = document.createElement('p');
+         raisedFund.setAttribute('id','raisedFund');
+         var number = data.raised;
+         raisedFund.innerHTML  = number.toLocaleString('en-IN', {
+            maximumFractionDigits: 2,
+            style: 'currency',
+            currency: 'INR'
+        }); 
+        
+
+        let progress_Bar = document.createElement('div');
+        progress_Bar.setAttribute('id','progressBar');
+      
+        let progress_done = document.createElement('div');
+        progress_done.setAttribute('id','progressDone');
+        
+       let widthPercentage = (data.raised/data.goal)*100;
+       let progressWidth = widthPercentage.toString() + "%";
+       progress_done.style.width = progressWidth;
+
+       progress_Bar.append(progress_done);
+
+        let textraised = document.createElement('p');
+        textraised.setAttribute('id',"txtraised");
+        textraised.innerText = "raised";
+
+
+        let daysTag = document.createElement('p');
+        daysTag.innerText = data.daysLeft;
+        daysTag.setAttribute('id','days');
+         
+        let text1 = document.createElement('p');
+        text1.innerText = "Days Left";
+        text1.setAttribute('id','txt1');
+
+
+        let supportTag = document.createElement('p');
+        supportTag.innerText = data.supporters;
+        supportTag.setAttribute('id','supporters');
+     
+        let text2 = document.createElement('p');
+        text2.innerText = "Supporters";
+        text2.setAttribute('id','txt2');
+
+         let innerDiv = document.createElement('a');
+         innerDiv.style.textDecoration = "none";
+         innerDiv.href = "#";
+         innerDiv.style.color = "inherit";
+
+         innerDiv.append(imageTag,titleTag,userLogo,raiser,raisedFund,textraised,progress_Bar,daysTag,text1,supportTag,text2);
+
+          document.getElementById('fundraisers').append(innerDiv);
+    });
+  }
+
   function showFundraisers(cat){
     
     const fundRaiserDiv = document.querySelector('#fundraisers');
@@ -67,7 +339,7 @@ var fund_raisers = [
 
     let raised_category = cat.innerText ;
   
-    let MyFundraisers = JSON.parse(localStorage.getItem("fund_raisers"));
+   // let MyFundraisers = JSON.parse(localStorage.getItem("fund_raisers"));
 
     MyFundraisers.forEach( function(data){
        
@@ -84,6 +356,7 @@ var fund_raisers = [
          
          let userLogo = document.createElement('img');
          userLogo.src = data.userImage;
+         userLogo.alt = "";
          userLogo.style.borderRadius = "50%";
          userLogo.style.width = "30px"
          userLogo.style.height = "30px";
@@ -151,5 +424,7 @@ var fund_raisers = [
        }
 
     });
-
+   
   }
+
+  showAllFundraisers();
