2b5f41331488b6f978ea2fa05dfd1f2975e46aa7
Mukul <howalemukul@gmail.com>
2021-09-10 02:42:06 +0530

    update on the donate page

--- donate/donate.js
+++ donate/donate.js
@@ -3,7 +3,7 @@ var para = new URLSearchParams(window.location.search);
 
 var pass  = para.get("fundRaiser");
 
-
+//data from the localstorage is appended to the page
 window.onload = function(){
   let MyfundraiserObjects  = JSON.parse(localStorage.getItem("fund_raisers"));
 
@@ -40,5 +40,67 @@ window.onload = function(){
    newpara.append("fundRaiser",pass);
    location.href = "donatefund.html?" + newpara.toString();
 
+ }
+
+ let changeDiv = document.getElementById('change');
+
+ let about = document.getElementById('about');
+ let updated = document.getElementById('updated');
+ let comments = document.getElementById('comments');
+
+ about.onclick = () =>{
+  about.style.borderBottom = "3px solid #01BFBD";
+  updated.style.borderBottom = "none";
+  comments.style.borderBottom = "none";
+
+  about.firstElementChild.style.color = "#01BFBD";
+  updated.firstElementChild.style.color = "gray";
+  comments.firstElementChild.style.color = "gray";
+
+  let div = document.createElement('div');
+  div.style.height = "1000px";
+  div.style.textAlign = "center";
+
+  let h2 = document.createElement('h2');
+  h2.textContent = "About the Fundraiser";
+
+  div.append(h2);
+
+  changeDiv.replaceChildren(div)
+
+ }
+
+ 
+
+ updated.onclick = () =>{
+  updated.style.borderBottom = "3px solid #01BFBD";
+  comments.style.borderBottom = "none";
+  about.style.borderBottom = "none";
+
+  about.firstElementChild.style.color = "gray";
+  updated.firstElementChild.style.color = "#01BFBD";
+  comments.firstElementChild.style.color = "gray";
+
+  let div = document.createElement('div');
+  div.style.height = "200px";
+
+  changeDiv.replaceChildren(div);
+ }
+
+ 
+
+ comments.onclick = () =>{
+  comments.style.borderBottom = "3px solid #01BFBD";
+  updated.style.borderBottom = "none";
+  about.style.borderBottom = "none";
+
+  about.firstElementChild.style.color = "gray";
+  updated.firstElementChild.style.color = "gray";
+  comments.firstElementChild.style.color = "#01BFBD"; 
+
+  let div = document.createElement('div');
+  div.style.height = "400px";
+
+  changeDiv.replaceChildren(div);
  }
 
